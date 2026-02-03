da0df1276ff1810a6067e6400a6e778da96e10fb
Gabriel André <gabrielandre699@gmail.com>
2026-02-03 10:59:28 -0300

    refactor: reestruturação de pastas do core para melhor organização

---
+++ lib/app/app.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+// app.dart: o “widget raiz” (onde fica o MaterialApp, tema, rotas, providers etc.)
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HistoFish',
+      locale: const Locale("pt", "BR"),
+      theme: ThemeData(
+        // This is the theme of your application.
+       
+        colorScheme: .fromSeed(seedColor: Colors.blue),
+      ),
+      home: const MyHomePage(title: 'HistoFish Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+       
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
